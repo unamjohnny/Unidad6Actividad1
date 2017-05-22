df35045a28c8c82e454b99cd645aef8e0da0e1d6
Juan Carlos Carranza Moreno <accigamer77@hotmail.com>
2017-05-21 19:28:20 -0500

    Primer Commit

---
+++ app/src/main/java/com/example/johnny/unidad6actividad1/MainActivityUnidad6Actividad1.java
@@ -0,0 +1,14 @@
+package com.example.johnny.unidad6actividad1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityUnidad6Actividad1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_unidad6_actividad1);
+    }
+}
+
